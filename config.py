349a53a762dddf39c6dd09ec237b2ed1b116bced
Moaaz Mostafa <madh9894@gmail.com>
2025-05-16 01:01:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,26 @@
+import os
+
 class Config:
-    SECRET_KEY = 'your_secret_key'  
-    DEBUG = False  # Set to False for production
-
-    # These should be set as environment variables in Vercel
-    CLIENT_ID = "23QCSZ"
-    CLIENT_SECRET = "4112a6a244bf91db710ff2580f2be515"
+    # Get SECRET_KEY from environment or use default for development
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev_secret_key_replace_in_production')
     
-    # Update this to your Vercel deployment URL
-    REDIRECT_URI = "https://your-app-name.vercel.app/callback"
+    # Set DEBUG based on environment
+    DEBUG = os.environ.get('ENVIRONMENT', 'development') == 'development'
 
+    # Fitbit API credentials
+    CLIENT_ID = os.environ.get('CLIENT_ID', '23QCSZ')
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET', '4112a6a244bf91db710ff2580f2be515')
+    
+    # Auth URLs
     AUTH_URL = "https://www.fitbit.com/oauth2/authorize"
     TOKEN_URL = "https://api.fitbit.com/oauth2/token"
     API_BASE_URL = "https://api.fitbit.com/1/user/-/"
+    
+    # Dynamic REDIRECT_URI based on environment
+    VERCEL_URL = os.environ.get('VERCEL_URL', '')
+    
+    # If running on Vercel, use the Vercel URL, otherwise use localhost
+    if VERCEL_URL:
+        REDIRECT_URI = f"https://{VERCEL_URL}/callback"
+    else:
+        REDIRECT_URI = os.environ.get('REDIRECT_URI', 'http://localhost:5000/callback')
