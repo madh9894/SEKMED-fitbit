927952ffb8e6acbb760fdcaec9f431e9120d21e6
Moaaz Mostafa <madh9894@gmail.com>
2025-05-16 00:31:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
-class Config:
-    SECRET_KEY = 'your_secret_key'  
-    DEBUG = True
+import os
 
-    CLIENT_ID = "23QCSZ"
-    CLIENT_SECRET = "4112a6a244bf91db710ff2580f2be515"
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
+    DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
+    CLIENT_ID = os.environ.get('CLIENT_ID', "23QCSZ")
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET', "4112a6a244bf91db710ff2580f2be515")
     
     # Update these URLs for production
-    REDIRECT_URI = "https://your-vercel-app.vercel.app/callback"
-    FRONTEND_URL = "https://your-frontend-app.vercel.app"
+    REDIRECT_URI = os.environ.get('REDIRECT_URI', "https://sekmed-fitbit.vercel.app/callback")
+    FRONTEND_URL = os.environ.get('FRONTEND_URL', "https://sekmed-fitbit.vercel.app")
 
     AUTH_URL = "https://www.fitbit.com/oauth2/authorize"
     TOKEN_URL = "https://api.fitbit.com/oauth2/token"
